36dc4443f5776aa68bc3ad78f511c8176b9384df
haitz <a@a.a>
2024-04-16 13:44:35 +0200

    Adelantar la pagina principal, la parte grafica

--- src/Libreria/Paginas/AdminMainPage.java
+++ src/Libreria/Paginas/AdminMainPage.java
@@ -1,13 +1,13 @@
-package Libreria;
+package Libreria.Paginas;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class UserMainPage {
-    public UserMainPage(){
+public class AdminMainPage {
+    public AdminMainPage() {
         JFrame frame = new JFrame("Bibliopolis");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(900,600));
+        frame.setPreferredSize(new Dimension(900, 600));
 
         frame.setResizable(false);
         frame.setLayout(new BorderLayout());
@@ -16,9 +16,9 @@ public class UserMainPage {
         JPanel header = new JPanel();
         header.setBackground(Color.BLACK);
         header.setLayout(new BorderLayout());
-        header.setPreferredSize(new Dimension(700,100));
+        header.setPreferredSize(new Dimension(700, 100));
 
-        ImageIcon logo = new ImageIcon("src/imagenes/logo_blanco.png");
+        ImageIcon logo = new ImageIcon("src/Libreria/imagenes/logo_blanco.png");
         JLabel etiquetaFoto1 = new JLabel(logo);
 
         JLabel ayuda = new JLabel("Ayuda con...");
@@ -26,29 +26,33 @@ public class UserMainPage {
         JLabel eventosYNoticias = new JLabel("Eventos y Noticias");
         JLabel sobreNosotros = new JLabel("Visitas y Sobre nosotros");
 
-        ImageIcon userLogedIcon = new ImageIcon(("src/imagenes/user_icon_white_resize.png"));
+        ImageIcon userLogedIcon = new ImageIcon(("src/Libreria/imagenes/user_icon_white_resize.png"));
         JLabel inicioSesion = new JLabel(userLogedIcon);
 
-        JLabel underUser = new JLabel("User");
-
-        ayuda.setForeground(Color.WHITE);
-        colecciones.setForeground(Color.WHITE);
-        eventosYNoticias.setForeground(Color.WHITE);
-        sobreNosotros.setForeground(Color.WHITE);
-        inicioSesion.setForeground(Color.WHITE);
-        underUser.setForeground(Color.WHITE);
-
+        JLabel underUser = new JLabel("Admin");
 
         ayuda.setFont(new Font("Arial",Font.BOLD,14));
+        ayuda.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         colecciones.setFont(new Font("Arial",Font.BOLD,14));
+        colecciones.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         eventosYNoticias.setFont(new Font("Arial",Font.BOLD,14));
+        eventosYNoticias.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         sobreNosotros.setFont(new Font("Arial",Font.BOLD,14));
-        underUser.setFont(new Font("Arial",Font.BOLD,14));
+        sobreNosotros.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        inicioSesion.setFont(new Font("Arial",Font.BOLD,14));
+        inicioSesion.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 
-        JPanel vertical= new JPanel();
+
+        ayuda.setFont(new Font("Arial", Font.BOLD, 14));
+        colecciones.setFont(new Font("Arial", Font.BOLD, 14));
+        eventosYNoticias.setFont(new Font("Arial", Font.BOLD, 14));
+        sobreNosotros.setFont(new Font("Arial", Font.BOLD, 14));
+        underUser.setFont(new Font("Arial", Font.BOLD, 14));
+
+        JPanel vertical = new JPanel();
         vertical.setLayout(new BoxLayout(vertical, BoxLayout.Y_AXIS));
         vertical.setLayout(new FlowLayout());
-        vertical.setPreferredSize(new Dimension(50,50));
+        vertical.setPreferredSize(new Dimension(50, 50));
         vertical.setBackground(Color.BLACK);
         vertical.add(inicioSesion);
         vertical.add(underUser);
@@ -56,7 +60,7 @@ public class UserMainPage {
         JPanel grupoBotones = new JPanel();
         grupoBotones.setBackground(Color.BLACK);
         grupoBotones.setLayout(new BoxLayout(grupoBotones, BoxLayout.X_AXIS));
-        grupoBotones.setPreferredSize(new Dimension(200,100));
+        grupoBotones.setPreferredSize(new Dimension(200, 100));
         grupoBotones.add(Box.createHorizontalStrut(40));
         grupoBotones.add(ayuda);
         grupoBotones.add(Box.createHorizontalStrut(40));
@@ -69,12 +73,6 @@ public class UserMainPage {
         grupoBotones.add(vertical);
 
 
-
-
-
-
-
-
         JPanel headerPanel = new JPanel(new BorderLayout());
         headerPanel.setBackground(Color.BLACK);
         headerPanel.add(grupoBotones, BorderLayout.CENTER);
@@ -82,10 +80,9 @@ public class UserMainPage {
         header.add(headerPanel, BorderLayout.CENTER);
 
 
-
         //Body
         JPanel body = new JPanel();
-        body.setPreferredSize(new Dimension(700,500));
+        body.setPreferredSize(new Dimension(700, 500));
 
 
         frame.add(header, BorderLayout.NORTH);
@@ -95,7 +92,6 @@ public class UserMainPage {
         frame.setVisible(true);
     }
     public static void main(String[] args){
-        UserMainPage ver = new UserMainPage();
+        AdminMainPage ver = new AdminMainPage();
     }
-
 }
--- src/Libreria/Paginas/UserMainPage.java
+++ src/Libreria/Paginas/UserMainPage.java
@@ -1,13 +1,13 @@
-package Libreria;
+package Libreria.Paginas;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class AdminMainPage {
-    public AdminMainPage() {
+public class UserMainPage {
+    public UserMainPage(){
         JFrame frame = new JFrame("Bibliopolis");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(900, 600));
+        frame.setPreferredSize(new Dimension(900,600));
 
         frame.setResizable(false);
         frame.setLayout(new BorderLayout());
@@ -16,9 +16,9 @@ public class AdminMainPage {
         JPanel header = new JPanel();
         header.setBackground(Color.BLACK);
         header.setLayout(new BorderLayout());
-        header.setPreferredSize(new Dimension(700, 100));
+        header.setPreferredSize(new Dimension(700,100));
 
-        ImageIcon logo = new ImageIcon("src/imagenes/logo_blanco.png");
+        ImageIcon logo = new ImageIcon("src/Libreria/imagenes/logo_blanco.png");
         JLabel etiquetaFoto1 = new JLabel(logo);
 
         JLabel ayuda = new JLabel("Ayuda con...");
@@ -26,29 +26,35 @@ public class AdminMainPage {
         JLabel eventosYNoticias = new JLabel("Eventos y Noticias");
         JLabel sobreNosotros = new JLabel("Visitas y Sobre nosotros");
 
-        ImageIcon userLogedIcon = new ImageIcon(("src/imagenes/user_icon_white_resize.png"));
+        ImageIcon userLogedIcon = new ImageIcon(("src/Libreria/imagenes/user_icon_white_resize.png"));
         JLabel inicioSesion = new JLabel(userLogedIcon);
 
-        JLabel underUser = new JLabel("Admin");
-
-        ayuda.setForeground(Color.WHITE);
-        colecciones.setForeground(Color.WHITE);
-        eventosYNoticias.setForeground(Color.WHITE);
-        sobreNosotros.setForeground(Color.WHITE);
-        inicioSesion.setForeground(Color.WHITE);
-        underUser.setForeground(Color.WHITE);
+        JLabel underUser = new JLabel("User");
 
 
-        ayuda.setFont(new Font("Arial", Font.BOLD, 14));
-        colecciones.setFont(new Font("Arial", Font.BOLD, 14));
-        eventosYNoticias.setFont(new Font("Arial", Font.BOLD, 14));
-        sobreNosotros.setFont(new Font("Arial", Font.BOLD, 14));
-        underUser.setFont(new Font("Arial", Font.BOLD, 14));
+        ayuda.setFont(new Font("Arial",Font.BOLD,14));
+        ayuda.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        colecciones.setFont(new Font("Arial",Font.BOLD,14));
+        colecciones.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        eventosYNoticias.setFont(new Font("Arial",Font.BOLD,14));
+        eventosYNoticias.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        sobreNosotros.setFont(new Font("Arial",Font.BOLD,14));
+        sobreNosotros.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        inicioSesion.setFont(new Font("Arial",Font.BOLD,14));
+        inicioSesion.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 
-        JPanel vertical = new JPanel();
+
+
+        ayuda.setFont(new Font("Arial",Font.BOLD,14));
+        colecciones.setFont(new Font("Arial",Font.BOLD,14));
+        eventosYNoticias.setFont(new Font("Arial",Font.BOLD,14));
+        sobreNosotros.setFont(new Font("Arial",Font.BOLD,14));
+        underUser.setFont(new Font("Arial",Font.BOLD,14));
+
+        JPanel vertical= new JPanel();
         vertical.setLayout(new BoxLayout(vertical, BoxLayout.Y_AXIS));
         vertical.setLayout(new FlowLayout());
-        vertical.setPreferredSize(new Dimension(50, 50));
+        vertical.setPreferredSize(new Dimension(50,50));
         vertical.setBackground(Color.BLACK);
         vertical.add(inicioSesion);
         vertical.add(underUser);
@@ -56,7 +62,7 @@ public class AdminMainPage {
         JPanel grupoBotones = new JPanel();
         grupoBotones.setBackground(Color.BLACK);
         grupoBotones.setLayout(new BoxLayout(grupoBotones, BoxLayout.X_AXIS));
-        grupoBotones.setPreferredSize(new Dimension(200, 100));
+        grupoBotones.setPreferredSize(new Dimension(200,100));
         grupoBotones.add(Box.createHorizontalStrut(40));
         grupoBotones.add(ayuda);
         grupoBotones.add(Box.createHorizontalStrut(40));
@@ -69,6 +75,12 @@ public class AdminMainPage {
         grupoBotones.add(vertical);
 
 
+
+
+
+
+
+
         JPanel headerPanel = new JPanel(new BorderLayout());
         headerPanel.setBackground(Color.BLACK);
         headerPanel.add(grupoBotones, BorderLayout.CENTER);
@@ -76,9 +88,10 @@ public class AdminMainPage {
         header.add(headerPanel, BorderLayout.CENTER);
 
 
+
         //Body
         JPanel body = new JPanel();
-        body.setPreferredSize(new Dimension(700, 500));
+        body.setPreferredSize(new Dimension(700,500));
 
 
         frame.add(header, BorderLayout.NORTH);
@@ -88,6 +101,7 @@ public class AdminMainPage {
         frame.setVisible(true);
     }
     public static void main(String[] args){
-        AdminMainPage ver = new AdminMainPage();
+        UserMainPage ver = new UserMainPage();
     }
+
 }
