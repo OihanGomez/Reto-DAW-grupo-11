7c805b3df2643b3d1e8df5b3fe89aae6465bc8c8
Oihan Gomez <ogomezaz23dw@ikzubirimanteo.com>
2024-04-29 09:22:58 +0200

    Mostrar informacion en panel correcto.

--- src/Libreria/Paginas/Principal.java
+++ src/Libreria/Paginas/Principal.java
@@ -161,9 +161,9 @@ public class Principal {
 
                 if (libro != null) {
                     LibroInfo libroInfo = new LibroInfo(libro);
-                    abajoContenido.removeAll(); // Limpiar cualquier componente existente en el panel
-                    abajoContenido.add(libroInfo);
-                    abajoContenido.revalidate(); // Actualizar el panel para mostrar la nueva tarjeta del libro
+                    panelIzquerda.removeAll(); // Limpiar cualquier componente existente en el panel
+                    panelIzquerda.add(libroInfo);
+                    panelIzquerda.revalidate(); // Actualizar el panel para mostrar la nueva tarjeta del libro
                 } else {
                     // Si el libro no se encuentra, mostrar un mensaje
                     JOptionPane.showMessageDialog(null, "El libro no fue encontrado.", "Error", JOptionPane.ERROR_MESSAGE);
