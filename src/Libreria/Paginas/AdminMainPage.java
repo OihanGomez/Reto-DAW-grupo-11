c034fc4be8618cd3516d84d964ba6e0297a953fe
ik012982i9 <a@a.a>
2024-04-29 09:23:18 +0200

    adición de un editor de usuarios dentro de la pagina del administrador

--- src/Libreria/Paginas/AdminMainPage.java
+++ src/Libreria/Paginas/AdminMainPage.java
@@ -1,6 +1,8 @@
 package Libreria.Paginas;
 
 import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 
 public class AdminMainPage {
@@ -87,11 +89,68 @@ public class AdminMainPage {
         headerPanel.add(etiquetaFoto1, BorderLayout.WEST);
         header.add(headerPanel, BorderLayout.CENTER);
 
+        //Contenido que va dentro del panel "panelEdit"
+        JComboBox<String> comboBox = new JComboBox<>();
+        comboBox.setBounds(50,50,200,30);
+        JTextField direccion = new JTextField();
+        direccion.setBounds(50,100,200,30);
+        JTextField nombre = new JTextField();
+        nombre.setBounds(50,150,200,30);
+        JTextField apellido = new JTextField();
+        apellido.setBounds(50,200,200,30);
+        JTextField correo = new JTextField();
+        correo.setBounds(50,250,200,30);
+        JTextField contraseña = new JTextField();
+        contraseña.setBounds(50,300,200,30);
+        JLabel direccionEtiqueta = new JLabel("Dirección:");
+        direccionEtiqueta.setBounds(50,77,150,30);
+        JLabel nombreEtiqueta = new JLabel("Nombre:");
+        nombreEtiqueta.setBounds(50,127,150,30);
+        JLabel apellidoEtiqueta = new JLabel("Apellido:");
+        apellidoEtiqueta.setBounds(50,177,150,30);
+        JLabel correoEtiqueta = new JLabel("Correo:");
+        correoEtiqueta.setBounds(50,227,150,30);
+        JLabel contraseñaEtiqueta = new JLabel("Contraseña:");
+        contraseñaEtiqueta.setBounds(50,277,150,30);
+        JLabel aceptarEdit = new JLabel("Aceptar cambios");
+        aceptarEdit.setBounds(100,350,150,30);
 
-        //Cuerpo
+        //Formatos de las etiquetas
+        direccionEtiqueta.setFont(new Font("Arial",Font.BOLD,14));
+        nombreEtiqueta.setFont(new Font("Arial",Font.BOLD,14));
+        apellidoEtiqueta.setFont(new Font("Arial",Font.BOLD,14));
+        correoEtiqueta.setFont(new Font("Arial",Font.BOLD,14));
+        contraseñaEtiqueta.setFont(new Font("Arial",Font.BOLD,14));
+        aceptarEdit.setFont(new Font("Arial",Font.BOLD,14));
+        aceptarEdit.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+
+        //Panel de detro del panel "body"
+        JPanel panelEdit = new JPanel();
+        panelEdit.setBounds(300,25,300,400);
+        panelEdit.setLayout(null);
+        panelEdit.add(comboBox);
+        panelEdit.add(correo);
+        panelEdit.add(nombre);
+        panelEdit.add(contraseña);
+        panelEdit.add(aceptarEdit);
+        panelEdit.add(apellido);
+        panelEdit.add(direccion);
+        panelEdit.add(direccionEtiqueta);
+        panelEdit.add(nombreEtiqueta);
+        panelEdit.add(apellidoEtiqueta);
+        panelEdit.add(correoEtiqueta);
+        panelEdit.add(contraseñaEtiqueta);
+
+
+        //Panel del cuerpo
         JPanel body = new JPanel();
         body.setPreferredSize(new Dimension(700, 500));
+        body.setLayout(null);
+        body.add(panelEdit);
 
+        //Lineas que rodean el panel "panelEdit"
+        panelEdit.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED), "Usuarios",
+                TitledBorder.CENTER, TitledBorder.DEFAULT_POSITION));
 
         frame.add(header, BorderLayout.NORTH);
         frame.add(body, BorderLayout.CENTER);
